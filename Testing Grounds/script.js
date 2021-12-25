f6974038aab533c823279929c329ae5596c49222
Cailan <cailanvr@gmail.com>
2021-12-25 13:12:50 -0600

    initial element creation test

--- Testing Grounds/script.js
+++ Testing Grounds/script.js
@@ -1,6 +1,6 @@
 function create() {
     var dupe = document.createElement("DIV");
     dupe.setAttribute("id","holder")
+    dupe.innerHTML = "Duped";
     document.getElementById("main").appendChild(dupe);
-
 }
